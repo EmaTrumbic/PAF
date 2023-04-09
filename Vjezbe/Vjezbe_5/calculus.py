65a49e8150f9f425cf5ed5daca57391e613b9cd3
EmaTrumbic <etrumbic@pmfst.hr>
2023-04-09 20:37:46 +0200

    V5

--- Vjezbe/Vjezbe_5/calculus.py
+++ Vjezbe/Vjezbe_5/calculus.py
@@ -1,19 +1,31 @@
 import numpy as np
 
-def derivation(f, x, dx, use2PointMethod):
+def point_derivation(f, x, dx, use2PointMethod=False):
     if use2PointMethod == True:
         return (f(x+dx)-f(x))/dx
     else:
         return (f(x+dx)-f(x-dx))/(2*dx)
 
-def derivation2(f, a, b, dx, use2PointMethod):
-    lista_tocaka = np.linspace(a, b, 40)
-    lista_derivacija = []
+def list_derivation(f, a, b, dx, use2PointMethod=False):
+    lista_tocaka = np.arange(a, b, dx); lista_derivacija = []
     if use2PointMethod == True:
-        for el in lista_tocaka:
-            lista_derivacija.append((f(el+dx)-f(el))/dx)
+        for x in lista_tocaka:
+            lista_derivacija.append((f(x+dx)-f(x))/dx)
     else:
-        for el in lista_tocaka:
-            lista_derivacija.append((f(el+dx)-f(el-dx))/(2*dx))
+        for x in lista_tocaka:
+            lista_derivacija.append((f(x+dx)-f(x-dx))/(2*dx))
     return lista_tocaka, lista_derivacija
 
+def integration(f, a, b, n):
+    lista = np.linspace(a, b, n); donji_int = 0; gornji_int = 0
+    dx = lista[1]-a
+    for x in range(len(lista)-1):
+        donji_int += f(lista[x])*abs(dx); gornji_int += f(lista[x+1])*abs(dx)
+    return donji_int, gornji_int
+
+def trapezoidal_integration(f, a, b, n):
+    lista = np.linspace(a, b, n); integral = 0
+    dx = lista[1]-a
+    for x in range(len(lista)-1):
+        integral += (f(lista[x])+f(lista[x+1]))*abs(dx)/2
+    return integral
--- Vjezbe/Vjezbe_5/zdk.py
+++ Vjezbe/Vjezbe_5/zdk.py
@@ -1,20 +1,26 @@
-import calculus as cal
-import matplotlib.pyplot as plt
-import numpy as np
+import calculus as cal; import matplotlib.pyplot as plt; import numpy as np
 
 def f1(x):
     return 5*(x**3)-2*(x**2)+2*x-3
-
 def df1(x):
     return 15*(x**2)-4*x+2
-
 def f2(x):
     return np.sin(x)
+def f3(x):
+    return 2*(x**2)+3
 
-print(cal.derivation2(f1, 1, 10, 0.1, False))
-print(cal.derivation2(f1, 1, 10, 0.1, True))
 
-plt.plot(cal.derivation2(f1, -2, 2, 0.1,)[0], df1(cal.derivation2(f1, -2, 2, 0.1)[0]))
-plt.plot(cal.derivation2(f1, -2, 2, 0.1)[0], cal.derivation2(f1, -2, 2, 0.1)[1], 'ro')
-plt.plot(cal.derivation2(f1, -2, 2, 0.01)[0], cal.derivation2(f1, -2, 2, 0.01)[1], 'go')
-plt.show()
+print('Derivacija kubne funkcije u jednoj tocki: {}'.format(cal.point_derivation(f1, 4, 0.1))), print('Lista brojeva: {}\nIznos derivacija trigonometrijske funkcije: {}'.format(cal.list_derivation(f2, 1, 10, 0.1, True)[0], cal.list_derivation(f2, 1, 10, 0.1, True)[1]))
+print('Donja meda: {}, gornja meda: {}'.format(cal.integration(f1, 0, 5, 100)[0],cal.integration(f1, 0, 5, 100)[1])), print('Trapezoidna integracija: {}'.format(cal.trapezoidal_integration(f1, 0, 5, 100)))
+
+
+plt.plot(cal.list_derivation(f1, -2, 2, 0.1)[0], df1(cal.list_derivation(f1, -2, 2, 0.1)[0])), plt.plot(cal.list_derivation(f1, -2, 2, 0.1)[0], cal.list_derivation(f1, -2, 2, 0.1)[1], 'go'), plt.plot(cal.list_derivation(f1, -2, 2, 0.5)[0], cal.list_derivation(f1, -2, 2, 0.5)[1], 'ro')
+plt.title('Derivacija'), plt.xlabel('Tocke'), plt.ylabel('Derivacija u tockama'), plt.show()
+
+
+lista = []
+for n in np.linspace(100, 1000, 21):
+    plt.plot(n, cal.integration(f3, 0, 1, int(n))[0], 'ro'); plt.plot(n, cal.integration(f3, 0, 1, int(n))[1], 'bo'); plt.plot(n, cal.trapezoidal_integration(f3, 0, 1, int(n)), 'go')
+    lista.append(11/3)
+plt.plot(np.linspace(100, 1000, 21), lista)
+plt.title('Integracija'), plt.xlabel('Broj koraka'), plt.ylabel('Integral'), plt.show()
