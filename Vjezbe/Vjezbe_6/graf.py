6e477dc876777b23f67d6a5890c8919f02890f44
EmaTrumbic <etrumbic@pmfst.hr>
2023-04-23 11:37:09 +0200

    V6

--- Vjezbe/Vjezbe_6/graf.py
+++ Vjezbe/Vjezbe_6/graf.py
@@ -2,21 +2,30 @@ import harmonic_oscillator as h
 import matplotlib.pyplot as plt
 import numpy as np
 
-h1 = h.HarmonicOscillator(2,2,3,1,0.01)
-h2 = h.HarmonicOscillator(2,2,3,1,0.1)
+h1 = h.HarmonicOscillator(3,2,3,1,0.01)
+h2 = h.HarmonicOscillator(3,2,3,1,0.1)
+h3 = h.HarmonicOscillator(3,2,3,1,0.2)
+h4 = h.HarmonicOscillator(3,2,3,1,0.5)
 
-t1 = np.arange(0, 20, 0.01)
-t2 = np.arange(0, 20, 0.1)
+h1.plot_trajectory(20)
+h2.move(20)
+h3.move(20)
+h4.move(20)
 
-for el in range(len(t1)-1):
-    h1.move()
-for el in range(len(t2)-1):
-    h2.move()
-
-plt.plot(h1.plot_analitical()[0], h1.plot_analitical()[1])
-plt.plot(t1, h1.returnInfo()[0], 'go', markersize=0.3)
-plt.plot(t2, h2.returnInfo()[0], 'ro', markersize=1)
+h1.plot_analitical()
+plt.plot(h1.returnInfo()[3], h1.returnInfo()[0], 'go', markersize=0.3)
+plt.plot(h2.returnInfo()[3], h2.returnInfo()[0], 'ro', markersize=1)
 plt.title('Preciznost')
 plt.xlabel('t [s]')
 plt.ylabel('x [m]')
+plt.show()
+
+analiticki_period = 2*np.pi*np.sqrt(h1.returnInfo()[-1]/h1.returnInfo()[-2])
+print('Analiticki period: {}'.format(analiticki_period))
+
+plt.plot([0.01, 0.1, 0.2, 0.5], [analiticki_period, analiticki_period, analiticki_period, analiticki_period])
+plt.plot([0.01, 0.1, 0.2, 0.5], [h1.numericalPeriod(), h2.numericalPeriod(), h3.numericalPeriod(), h4.numericalPeriod()], 'o')
+plt.xlabel('Korak')
+plt.ylabel('Numericki period')
+plt.title('Preciznost numerickog perioda')
 plt.show()
--- Vjezbe/Vjezbe_6/harmonic_oscillator.py
+++ Vjezbe/Vjezbe_6/harmonic_oscillator.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 class HarmonicOscillator:
-    def __init__(self, x, v, m ,k, dt):
+    def __init__(self, x, v, m, k, dt):
         self.x = x
         self.v = v
         self.k = k
@@ -10,44 +10,61 @@ class HarmonicOscillator:
         self.lista_x = [x]
         self.lista_v = [v]
         self.lista_a = [-1*self.k*self.x/self.m]
+        self.lista_t = [0]
     
     def reset(self):
         self.lista_x = [self.x]
         self.lista_v = [self.v]
         self.lista_a = [-1*self.k*self.x/self.m]
+        self.lista_t = [0]
     
-    def move(self):
-        self.lista_x.append(self.lista_x[-1]+(self.lista_v[-1]*self.dt))
-        self.lista_v.append(self.lista_v[-1]+(self.lista_a[-1]*self.dt))
-        self.lista_a.append(-1*self.k*self.lista_x[-1]/self.m)
+    def move(self, t):
+        for i in np.arange(self.dt, t, self.dt):
+            self.lista_x.append(self.lista_x[-1]+(self.lista_v[-1]*self.dt))
+            self.lista_v.append(self.lista_v[-1]+(self.lista_a[-1]*self.dt))
+            self.lista_a.append(-1*self.k*self.lista_x[-1]/self.m)
+            self.lista_t.append(self.lista_t[-1]+self.dt)
     
     def returnInfo(self):
-        return self.lista_x, self.lista_v, self.lista_a
+        return self.lista_x, self.lista_v, self.lista_a, self.lista_t, self.k, self.m
     
     def plot_trajectory(self, t):
-        vrijeme = np.arange(0, t, self.dt)
-        for i in range(len(vrijeme)-1):
-            HarmonicOscillator.move(self, self.dt)
+        HarmonicOscillator.move(self, t)
         
         fig, axes = plt.subplots(nrows=3, ncols=1)
 
-        axes[0].plot(vrijeme, self.lista_x)
+        axes[0].plot(self.lista_t, self.lista_x)
         axes[0].set_title('x-t graf')
         axes[0].set_xlabel('t [s]')
         axes[0].set_ylabel('x [m]')
 
-        axes[1].plot(vrijeme, self.lista_v)
+        axes[1].plot(self.lista_t, self.lista_v)
         axes[1].set_title('v-t graf')
         axes[1].set_xlabel('t [s]')
         axes[1].set_ylabel('v [m/s]')
 
-        axes[2].plot(vrijeme, self.lista_a)
+        axes[2].plot(self.lista_t, self.lista_a)
         axes[2].set_title('a-t graf')
         axes[2].set_xlabel('t [s]')
         axes[2].set_ylabel('a [m/s^2]')
 
         plt.show()
     
+    def numericalPeriod(self):
+        HarmonicOscillator.reset(self)
+        HarmonicOscillator.move(self, 4*np.pi*np.sqrt(HarmonicOscillator.returnInfo(self)[-1]/HarmonicOscillator.returnInfo(self)[-2]))
+        x = -1
+        for i in range(len(self.lista_x)):
+            if x == -1:
+                if self.lista_x[0]/self.lista_x[i] < 0:
+                    x = 0
+                    t0 = self.lista_t[i]
+            if x == 0:
+                if self.lista_x[0]/self.lista_x[i] > 0:
+                    t1 = self.lista_t[i]
+                    break
+        return 2*(t1-t0)
+        
     def plot_analitical(self):
         A = np.sqrt((self.x)**2+((self.m/self.k)*(self.v)**2))
         fi = np.arcsin(self.x/A)
@@ -55,4 +72,4 @@ class HarmonicOscillator:
         lista = []
         for t in vrijeme:
             lista.append(A * np.sin(np.sqrt(self.k/self.m) * t + fi))
-        return vrijeme, lista
+        plt.plot(vrijeme, lista)
