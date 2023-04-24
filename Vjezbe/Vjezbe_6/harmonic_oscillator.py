7f7f81261cde81ebe6f2616ea2dd710ce99919fc
EmaTrumbic <etrumbic@pmfst.hr>
2023-04-24 09:53:44 +0200

    V6

--- Vjezbe/Vjezbe_6/harmonic_oscillator.py
+++ Vjezbe/Vjezbe_6/harmonic_oscillator.py
@@ -18,15 +18,15 @@ class HarmonicOscillator:
         self.lista_a = [-1*self.k*self.x/self.m]
         self.lista_t = [0]
     
+    def returnInfo(self):
+        return self.lista_x, self.lista_v, self.lista_a, self.lista_t, self.k, self.m
+    
     def move(self, t):
         for i in np.arange(self.dt, t, self.dt):
             self.lista_x.append(self.lista_x[-1]+(self.lista_v[-1]*self.dt))
             self.lista_v.append(self.lista_v[-1]+(self.lista_a[-1]*self.dt))
             self.lista_a.append(-1*self.k*self.lista_x[-1]/self.m)
             self.lista_t.append(self.lista_t[-1]+self.dt)
-    
-    def returnInfo(self):
-        return self.lista_x, self.lista_v, self.lista_a, self.lista_t, self.k, self.m
     
     def plot_trajectory(self, t):
         HarmonicOscillator.move(self, t)
@@ -49,27 +49,29 @@ class HarmonicOscillator:
         axes[2].set_ylabel('a [m/s^2]')
 
         plt.show()
-    
-    def numericalPeriod(self):
-        HarmonicOscillator.reset(self)
-        HarmonicOscillator.move(self, 4*np.pi*np.sqrt(HarmonicOscillator.returnInfo(self)[-1]/HarmonicOscillator.returnInfo(self)[-2]))
-        x = -1
-        for i in range(len(self.lista_x)):
-            if x == -1:
-                if self.lista_x[0]/self.lista_x[i] < 0:
-                    x = 0
-                    t0 = self.lista_t[i]
-            if x == 0:
-                if self.lista_x[0]/self.lista_x[i] > 0:
-                    t1 = self.lista_t[i]
-                    break
-        return 2*(t1-t0)
-        
-    def plot_analitical(self):
+
+    def plot_analitical(self, t, showPlot = True):
         A = np.sqrt((self.x)**2+((self.m/self.k)*(self.v)**2))
         fi = np.arcsin(self.x/A)
-        vrijeme = np.arange(0, 20, 0.1)
+        vrijeme = np.arange(0, t, self.dt)
         lista = []
         for t in vrijeme:
             lista.append(A * np.sin(np.sqrt(self.k/self.m) * t + fi))
         plt.plot(vrijeme, lista)
+        if showPlot == True:
+            plt.show()
+    
+    def numericalPeriod(self):
+        HarmonicOscillator.reset(self)
+        HarmonicOscillator.move(self, 4*np.pi*np.sqrt(HarmonicOscillator.returnInfo(self)[-1]/HarmonicOscillator.returnInfo(self)[-2]))
+        z = False
+        for i in range(len(self.lista_x)):
+            if z == False:
+                if self.lista_x[0]/self.lista_x[i] < 0:
+                    z = True
+                    t0 = self.lista_t[i]
+            if z == True:
+                if self.lista_x[0]/self.lista_x[i] > 0:
+                    t1 = self.lista_t[i]
+                    break
+        return 2*(t1-t0)
