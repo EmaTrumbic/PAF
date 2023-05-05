d75da2051a1b30d5eab31abac6830f065cf74b63
Borna <bpinteric@pmfst.hr>
2023-05-05 10:56:38 +0200

    V7

--- Vjezbe/Vjezbe_7/graf.py
+++ Vjezbe/Vjezbe_7/graf.py
@@ -4,12 +4,21 @@ import projectile as p
 
 p1 = p.Projectile(0.,0.,50.,45.,2.,1.22521,0.2,0.02,0.001)
 p2 = p.Projectile(0.,0.,50.,45.,2.,1.22521,0.2,0.02,0.01)
-p3 = p.Projectile(0.,0.,50.,45.,2.,1.22521,0.,0.02,0.001)
+p3 = p.Projectile(0.,0.,50.,45.,2.,1.22521,0.2,0.02,0.2)
+p4 = p.Projectile(0.,0.,50.,45.,2.,1.22521,0.,0.02,0.001)
 
 p1.plot_trajectory(False)
 p2.plot_trajectory(False)
 p2.reset()
-p2.plot_trajectory(True)
+p2.plot_trajectory()
 p3.plot_trajectory(False)
-plt.legend(['dt = 0.001', 'dt = 0.01', 'dt = 0.01 (Runge-Kutta)', 'dt = 0.001, Cd = 0'])
+p3.reset()
+p3.plot_trajectory()
+p4.plot_trajectory(False)
+p4.reset()
+p4.plot_trajectory()
+plt.legend(['dt = 0.001 s', 'dt = 0.01 s', 'dt = 0.01 s (RK)', 'dt = 0.2 s', 'dt = 0.2 s (RK)', 'dt = 0.001 s, Cd = 0', 'dt = 0.001 s, Cd = 0 (RK)'])
 plt.show()
+
+#Runge-Kutta metoda je preciznija od Euler-ove za isti korak, a naznake nefizikalnog gibanja se pojavljuju za dt = 0.2 s.
+#Za slucaj kada nemamo trenja, Euler-ovom metodom dobijemo malo veci domet i visinu u odnosu na Runge-Kutta metodu.
--- Vjezbe/Vjezbe_7/projectile.py
+++ Vjezbe/Vjezbe_7/projectile.py
@@ -13,18 +13,18 @@ class Projectile:
         self.dt = dt
         self.lista_x = [self.x]
         self.lista_y = [self.y]
-        self.lista_vx = [self.v * np.cos(self.kut)]
-        self.lista_vy = [self.v * np.sin(self.kut)]
-        self.lista_ax = [(-1*self.lista_vx[-1]/abs(self.lista_vx[-1]))*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vx[-1])**2]
-        self.lista_ay = [(-1*self.lista_vy[-1]/abs(self.lista_vy[-1]))*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vx[-1])**2]
+        self.lista_vx = [self.v*np.cos(self.kut)]
+        self.lista_vy = [self.v*np.sin(self.kut)]
+        self.lista_ax = [-1*np.sign(self.lista_vx[-1])*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vx[-1])**2]
+        self.lista_ay = [-9.81-np.sign(self.lista_vy[-1])*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vy[-1])**2]
     
     def reset(self):
         self.lista_x = [self.x]
         self.lista_y = [self.y]
-        self.lista_vx = [self.v * np.cos(self.kut)]
-        self.lista_vy = [self.v * np.sin(self.kut)]
-        self.lista_ax = [(-1*self.lista_vx[-1]/abs(self.lista_vx[-1]))*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vx[-1])**2]
-        self.lista_ay = [(-1*self.lista_vy[-1]/abs(self.lista_vy[-1]))*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vx[-1])**2]
+        self.lista_vx = [self.v*np.cos(self.kut)]
+        self.lista_vy = [self.v*np.sin(self.kut)]
+        self.lista_ax = [-1*np.sign(self.lista_vx[-1])*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vx[-1])**2]
+        self.lista_ay = [-9.81-np.sign(self.lista_vy[-1])*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vy[-1])**2]
 
     def returnInfo(self):
         return self.lista_x, self.lista_y, self.lista_vx, self.lista_vy, self.lista_ax, self.lista_ay
@@ -35,18 +35,18 @@ class Projectile:
             self.lista_y.append(self.lista_y[-1]+(self.lista_vy[-1]*self.dt))
             self.lista_vx.append(self.lista_vx[-1]+(self.lista_ax[-1]*self.dt))
             self.lista_vy.append(self.lista_vy[-1]+(self.lista_ay[-1]*self.dt))
-            self.lista_ax.append((-1*self.lista_vx[-1]/abs(self.lista_vx[-1]))*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vx[-1])**2)
-            self.lista_ay.append(-9.81-(self.lista_vy[-1]/abs(self.lista_vy[-1]))*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vy[-1])**2)
+            self.lista_ax.append(-1*np.sign(self.lista_vx[-1])*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vx[-1])**2)
+            self.lista_ay.append(-9.81-np.sign(self.lista_vy[-1])*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vy[-1])**2)
 
     def __moveRungeKutta(self):
         while self.lista_y[-1]>=0:
-            k_vx = [((-1*self.lista_vx[-1]/abs(self.lista_vx[-1]))*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vx[-1])**2)*self.dt]
-            k_vy = [(-9.81-(self.lista_vy[-1]/abs(self.lista_vy[-1]))*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vy[-1])**2)*self.dt]
+            k_vx = [(-1*np.sign(self.lista_vx[-1])*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vx[-1])**2)*self.dt]
+            k_vy = [(-9.81-np.sign(self.lista_vy[-1])*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vy[-1])**2)*self.dt]
             k_x = [self.lista_vx[-1]*self.dt]
             k_y = [self.lista_vy[-1]*self.dt]
             for i in range(3):
-                k_vx.append(((-1*(self.lista_vx[-1]+0.5*k_vx[-1])/abs(self.lista_vx[-1]+0.5*k_vx[-1]))*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vx[-1]+0.5*k_vx[-1])**2)*self.dt)
-                k_vy.append((-9.81-((self.lista_vy[-1]+0.5*k_vy[-1])/abs(self.lista_vy[-1]+0.5*k_vy[-1]))*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vy[-1]+0.5*k_vy[-1])**2)*self.dt)
+                k_vx.append((-1*np.sign(self.lista_vx[-1]+0.5*k_vx[-1])*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vx[-1]+0.5*k_vx[-1])**2)*self.dt)
+                k_vy.append((-9.81-np.sign(self.lista_vy[-1]+0.5*k_vy[-1])*(self.p*self.Cd*self.A/(2*self.m))*(self.lista_vy[-1]+0.5*k_vy[-1])**2)*self.dt)
                 k_x.append((self.lista_vx[-1]+0.5*k_vx[-1])*self.dt)
                 k_y.append((self.lista_vy[-1]+0.5*k_vy[-1])*self.dt)
             self.lista_vx.append(self.lista_vx[-1]+(k_vx[0]+2*k_vx[1]+2*k_vx[2]+k_vx[3])/6)
@@ -61,7 +61,7 @@ class Projectile:
             plt.title('x-y graf')
             plt.xlabel('x [m]')
             plt.ylabel('y [m]')
-        if useRungeKutta == False:
+        else:
             Projectile.__moveEuler(self)
             plt.plot(self.lista_x, self.lista_y)
             plt.title('x-y graf')
