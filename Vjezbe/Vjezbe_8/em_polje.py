ac67edda6005e09a28d05fb845e6cc17ced77be5
EmaTrumbic <etrumbic@pmfst.hr>
2023-05-11 19:05:11 +0200

    V8

--- Vjezbe/Vjezbe_8/em_polje.py
+++ Vjezbe/Vjezbe_8/em_polje.py
@@ -11,6 +11,9 @@ class Particle:
         self.v = [v]
         self.a = [self.q/self.m * (self.E + np.cross(self.v[-1],self.B))]
         self.dt = dt
+        self.x = []
+        self.y = []
+        self.z = []
     
     def reset(self):
         self.r = [self.r[0]]
@@ -25,10 +28,10 @@ class Particle:
             self.r.append(self.r[-1] + self.v[-1]*self.dt)
             self.v.append(self.v[-1] + self.a[-1]*self.dt)
             self.a.append(self.q/self.m * (self.E + np.cross(self.v[-1],self.B)))
-    
-    def evolve(self,t):
-        Particle.move(self,t)
-        return self.r
+        for i in range(len(self.r)):
+            self.x.append(self.r[i][0])
+            self.y.append(self.r[i][1])
+            self.z.append(self.r[i][2])
     
     def plot_trajectory(self,t):
         Particle.move(self, t)
@@ -40,7 +43,7 @@ class Particle:
             y.append(self.r[i][1])
             z.append(self.r[i][2])
         ax = plt.axes(projection='3d')
-        ax.plot(x,y,z)
+        ax.plot(self.x,self.y,self.z)
         ax.set_xlabel('x [m]')
         ax.set_ylabel('y [m]')
         ax.set_zlabel('z [m]')
--- Vjezbe/Vjezbe_8/test.py
+++ Vjezbe/Vjezbe_8/test.py
@@ -12,3 +12,28 @@ plt.show()
 e.plot_trajectory(20)
 plt.title('Elektron')
 plt.show()
+
+x1 = []
+y1 = []
+z1 = []
+x2 = []
+y2 = []
+z2 = []
+for i in range(len(p.returnInfo()[0])):
+    x1.append(p.returnInfo()[0][i][0])
+    y1.append(p.returnInfo()[0][i][1])
+    z1.append(p.returnInfo()[0][i][2])
+for i in range(len(e.returnInfo()[0])):
+    x2.append(e.returnInfo()[0][i][0])
+    y2.append(e.returnInfo()[0][i][1])
+    z2.append(e.returnInfo()[0][i][2])
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot3D(x1,y1,z1)
+ax.plot3D(x2,y2,z2)
+ax.set_title('Pozitron i elektron')
+ax.set_xlabel('x [m]')
+ax.set_ylabel('y [m]')
+ax.set_zlabel('z [m]')
+ax.legend(['Pozitron', 'Elektron'])
+plt.show()
