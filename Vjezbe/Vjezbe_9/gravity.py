f364fefc0d78a5c09a85d6e2482701cbd7820889
EmaTrumbic <etrumbic@pmfst.hr>
2023-05-17 22:56:18 +0200

    V9

--- Vjezbe/Vjezbe_9/gravity.py
+++ Vjezbe/Vjezbe_9/gravity.py
@@ -3,16 +3,14 @@ import numpy as np
 from numpy import linalg as LA
 class Gravity:
     def __init__(self,r1,v1,m1,r2,v2,m2,dt):
-        self.G = 6.67 * 10**(-11)
-        self.d12 = LA.norm(r1[0]-r2[0])
-        self.d21 = LA.norm(r2[0]-r1[0])
+        self.G = 6.67*10**(-11)
         self.r1 = [r1]
         self.v1 = [v1]
-        self.a1 = [-1*self.G*m2*self.d12/self.d12**3]
+        self.a1 = []
         self.m1 = m1
         self.r2 = [r2]
         self.v2 = [v2]
-        self.a2 = [-1*self.G*m1*self.d21/self.d21**3]
+        self.a2 = []
         self.m2 = m2
         self.dt = dt
         self.x1 = []
@@ -23,21 +21,20 @@ class Gravity:
     def reset(self):
         self.r1 = [self.r1[0]]
         self.v1 = [self.v1[0]]
-        self.a1 = [-1*self.G*self.m2*(self.r1[0]-self.r2[0])/np.dot((self.r1[0]-self.r2[0]),(self.r1[0]-self.r2[0]),(self.r1[0]-self.r2[0]))]
+        self.a1 = []
         self.r2 = [self.r2[0]]
         self.v2 = [self.v2[0]]
-        self.a2 = [-1*self.G*self.m1*(self.r2[0]-self.r1[0])/np.dot((self.r2[0]-self.r1[0]),(self.r2[0]-self.r1[0]),(self.r2[0]-self.r1[0]))]
+        self.a2 = []
     
     def returnInfo(self):
         return self.r1, self.v1, self.a1, self.r2, self.v2, self.a2,
     
     def move(self,t):
         for i in (self.dt, t, self.dt):
-            print((self.r1[-1]-self.r2[-1])**3)
-            self.a1.append(-1*self.G*self.m2*(self.r1[-1]-self.r2[-1])/np.dot((self.r1[-1]-self.r2[-1]),(self.r1[-1]-self.r2[-1]),(self.r1[-1]-self.r2[-1])))
+            self.a1.append(-1*self.G*self.m2*(self.r1[-1]-self.r2[-1])/(LA.norm(self.r1[-1]-self.r2[-1]))**3)
             self.v1.append(self.v1[-1] + self.a1[-1]*self.dt)
             self.r1.append(self.r1[-1] + self.v1[-1]*self.dt)
-            self.a2.append(-1*self.G*self.m1*(self.r2[-1]-self.r1[-1])/np.dot((self.r2[-1]-self.r1[-1]),(self.r2[-1]-self.r1[-1]),(self.r2[-1]-self.r1[-1])))
+            self.a2.append(-1*self.G*self.m1*(self.r2[-1]-self.r1[-2])/(LA.norm(self.r2[-1]-self.r1[-2]))**3)
             self.v2.append(self.v2[-1] + self.a2[-1]*self.dt)
             self.r2.append(self.r2[-1] + self.v2[-1]*self.dt)
     
@@ -48,6 +45,7 @@ class Gravity:
             self.y1.append(self.r1[i][1])
             self.x2.append(self.r2[i][0])
             self.y2.append(self.r2[i][1])
-        plt.plot(self.x1, self.y1)
-        plt.plot(self.x2, self.y2)
-        plt.show()
+        plt.plot(self.x1, self.y1, 'orange')
+        plt.plot(self.x2, self.y2, 'b')
+        plt.xlabel('x [m]')
+        plt.ylabel('y [m]')
